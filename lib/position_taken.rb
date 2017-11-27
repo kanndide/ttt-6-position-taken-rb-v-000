9879160aaf4e635ff8ef77ae19006e85cd0a3c76
William Kann <william.l.kann@gmail.com>
2017-11-27 17:50:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,8 @@
 # code your #position_taken? method here!
-board = ["","","","","","","","","",]
-index = #{board[]}
+
 def position_taken?(board, index)
   if index == "" || " " || nil
     false
-  elsif index == true
+  elsif index == 
   end
 end
