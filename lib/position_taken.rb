01125fccbaf95789d75f12c9f6a3329fd97d261d
William Kann <william.l.kann@gmail.com>
2017-11-27 18:10:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 
 
 def position_taken?(board, index)
-  if board[index] == "" || " " || nil
+  if #{board[index]} == "" || " " || nil
     false
-  elsif board[index] == "X" || "O"
+  elsif #{board[index]} == "X" || "O"
     true
   else
   end
